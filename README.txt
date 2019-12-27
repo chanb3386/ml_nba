533e9d003c1b33780f87c09a8a3dd9fc5fa0a36e
Brandon <chanx411@umn.edu>
2019-12-26 23:05:31 -0600

    created python model creation, need to increase data volume, make user app file

--- README.txt
+++ README.txt
@@ -1,38 +1,32 @@
 Brandon Chan
 
-To Do: update data function - updates data from web (?), neurtal network training, input data, prediction formula + user interaction
-final write up (conclusions, methodology, design, data inputs)
-loss function - for learning - (categorical_crossentropy works, look for better loss functions)
-activation function - used to propagate output of neuron forward
-cross validation (hold out)
-optimizer - used to minimize loss function (gradient descent?)
+Data from Kaggle and Basketball-Reference
 
+Data from the 2019-2020 season was extracted and the following data is input into a neural network for a single game:
+      ['FG_home', 'FGA_home', 'FG%_home', '3P_home', '3PA_home', '3P%_home',
+       '2P_home', '2PA_home', '2P%_home', 'FT_home', 'FTA_home', 'FT%_home',
+       'ORB_home', 'DRB_home', 'TRB_home', 'AST_home', 'STL_home', 'BLK_home',
+       'TOV_home', 'PF_home', 'PTS_home', 'FG_away', 'FGA_away', 'FG%_away',
+       '3P_away', '3PA_away', '3P%_away', '2P_away', '2PA_away', '2P%_away',
+       'FT_away', 'FTA_away', 'FT%_away', 'ORB_away', 'DRB_away', 'TRB_away',
+       'AST_away', 'STL_away', 'BLK_away', 'TOV_away', 'PF_away', 'PTS_away',
+       'FG_homeOppStat', 'FGA_homeOppStat', 'FG%_homeOppStat',
+       '3P_homeOppStat', '3PA_homeOppStat', '3P%_homeOppStat',
+       '2P_homeOppStat', '2PA_homeOppStat', '2P%_homeOppStat',
+       'FT_homeOppStat', 'FTA_homeOppStat', 'FT%_homeOppStat',
+       'ORB_homeOppStat', 'DRB_homeOppStat', 'TRB_homeOppStat',
+       'AST_homeOppStat', 'STL_homeOppStat', 'BLK_homeOppStat',
+       'TOV_homeOppStat', 'PF_homeOppStat', 'PTS_homeOppStat',
+       'FG_awayOppStats', 'FGA_awayOppStats', 'FG%_awayOppStats',
+       '3P_awayOppStats', '3PA_awayOppStats', '3P%_awayOppStats',
+       '2P_awayOppStats', '2PA_awayOppStats', '2P%_awayOppStats',
+       'FT_awayOppStats', 'FTA_awayOppStats', 'FT%_awayOppStats',
+       'ORB_awayOppStats', 'DRB_awayOppStats', 'TRB_awayOppStats',
+       'AST_awayOppStats', 'STL_awayOppStats', 'BLK_awayOppStats',
+       'TOV_awayOppStats', 'PF_awayOppStats', 'PTS_awayOppStats']
 
+Over the test sample, across 3 runs the average percent correct is: 56.8%
 
-want better optimier and cross validation
-may need to look into new loss func/activation func
-
-back prop: loss function, optimizer, metrics (accuracy - how right were you?)
-
-Testing: run the model over previous games and calculate the success? Maybe a better way of determining its success?
-
-
-network training: maybe need to create helper funcs to accquire/aggregate data
-
-Data collected: overall team stats, player-by-player team stats, home/away loss differential, recent team performance (W/L, performance
-relative to the mean, game info, etc), TEAM MISC, recent opp record
-
-neural network training: evaluation of season stats for team A and team B (ppg, fg%, 3pt%, ft%, fg/g, 3pt/g, ft/g) output W/L
--- take 2019-2020 games, train over that set, use 2019-2020 season stats for input, predict winner, loss func
-
-prediction: take season stats and recency stats - compile averages (some weighted avg), input and predict
-
-expected: at least 3 python files: getting/creating/cleaning up data, training, prediction and user interact (main)
-
-how to: save a model and use in a different file?
-
-all-star model (?): takes starters + bench as input
-training: more complex: how each player operated in each match up that can possibly appear on the field, need a way to show objective team benefit (+/-?)
---- need better single game metric
-
-DATA SOURCES: Kaggle & Basketball-Reference
+TODO:
+Data is currently only from 2019-2020 season: include 2014-2018 season data
+Add 2K rating as inputs for starting players
---
+++ src/create_train_model.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+LABELS = [True, False]
+# True = home win, False = away win
+LABEL_COLUMN = "WINorLOSS"
+
+TEAM_DICT = {
+             "MIL" : "Milwaukee Bucks",
+             "MIA" : "Miami Heat",
+             "BOS" : "Boston Celtics",
+             "TOR" : "Toronto Raptors",
+             "PHI" : "Philidelphia 76ers",
+             "IND" : "Indiana Pacers",
+             "BRK" : "Brooklyn Nets",
+             "ORL" : "Orlando Magic",
+             "CHO" : "Charlotte Hornets",
+             "CHI" : "Chicago Bulls",
+             "DET" : "Detroit Pistons",
+             "WAS" : "Washington Wizards",
+             "CLE" : "Cleveland Cavaliers",
+             "NYK" : "New York Knicks",
+             "ATL" : "Atlanta Hawks",
+             "LAL" : "Los Angeles Lakers",
+             "DEN" : "Denver Nuggets",
+             "HOU" : "Houston Rockets",
+             "LAC" : "Los Angeles Clippers",
+             "DAL" : "Dallas Mavericks",
+             "UTA" : "Utah Jazz",
+             "OKC" : "Oklahoma City Thunder",
+             "POR" : "Portland Trail Blazers",
+             "SAS" : "San Antonio Spurs",
+             "SAC" : "Sacramento Kings",
+             "PHO" : "Phoenix Suns",
+             "MEM" : "Memphis Grizzlies",
+             "MIN" : "Minnesota Timberwolves",
+             "NOP" : "New Orleans Pelicans",
+             "GSW" : "Golden State Warriors"
+            }
+
+BOOL_DICT = {1 : True, 0 : False}
+
+
+# FOR LATER USE - 2014-2018 data
+data2014_2018 = pd.read_csv("../data/games/nba-games-results-2014-2018.csv",index_col=1)
+data2014_2018 = data2014_2018.drop(columns=['Unnamed: 0','Game'])
+data2014_2018['Home'] = pd.Categorical(data2014_2018['Home'])
+data2014_2018['Home'] = data2014_2018.Home.cat.codes
+data2014_2018['WINorLOSS'] = pd.Categorical(data2014_2018['WINorLOSS'])
+data2014_2018['WINorLOSS'] = data2014_2018.WINorLOSS.cat.codes
+data2014_2018.index = data2014_2018.index.map(TEAM_DICT)
+data2014_2018['Opponent'] = data2014_2018['Opponent'].map(TEAM_DICT)
+#
+
+# Extracting NBA game results
+data19_20 = pd.read_csv("../data/games/nba-games-2019-oct-nov-dec.txt", index_col=4)
+data19_20 = data19_20.drop(columns=['Start (ET)','Unnamed: 6','Unnamed: 7', 'Attend.', 'Notes'])
+
+# Extracting NBA team season averages
+data_team_stat = pd.read_csv("../data/league-stats/team-per-game.txt", index_col=1)
+data_team_stat = data_team_stat.drop(columns=['Rk', 'G', 'MP'])
+
+# FOR LATER USE
+data2015 = pd.read_csv("../data/league-stats/team-per-game-14-15.txt",index_col=1)
+data2015 = data2015.drop(columns=['Rk','G', 'MP'])
+
+data2016 = pd.read_csv("../data/league-stats/team-per-game-15-16.txt",index_col=1)
+data2016 = data2016.drop(columns=['Rk','G', 'MP'])
+
+data2017 = pd.read_csv("../data/league-stats/team-per-game-16-17.txt",index_col=1)
+data2017 = data2017.drop(columns=['Rk','G', 'MP'])
+
+data2018 = pd.read_csv("../data/league-stats/team-per-game-17-18.txt",index_col=1)
+data2018 = data2018.drop(columns=['Rk','G', 'MP'])
+#
+
+# Extracting NBA Season opponent season averages
+dataOppStats2020 = pd.read_csv("../data/league-stats/opponent-stats.txt", index_col=1)
+dataOppStats2020 = dataOppStats2020.drop(columns=['Rk', 'G', 'MP'])
+
+# Creating inputs to Neural Network
+d5 = data19_20.join(data_team_stat) # adds home team stats
+d6 = d5.join(data_team_stat, on='Visitor/Neutral',lsuffix='_home',rsuffix='_away') # adds away team dataOppStats2020
+d7 = d6.join(dataOppStats2020) # adds home opponent season averages
+game_data = d7.join(dataOppStats2020, on='Visitor/Neutral',lsuffix='_homeOpp',rsuffix='_awayOpp') # adds away opp season averages
+game_data = game_data.drop(columns=['Date'])
+
+# randomizing rows so test, train samples from larger sample of teams
+row,col=game_data.shape
+game_data = game_data.sample(frac=1) # randomizing rows
+split = int(row * .2)
+train_data = game_data.iloc[0:split] # 20% of total data pool
+test_data = game_data.iloc[split:row]
+
+# getting training labels and cleaning training data
+train_labels = train_data["HomeWin"]
+train_labels = train_labels.to_numpy(dtype=bool)
+train_data = train_data.drop(columns=["HomeWin"])
+
+# test labels and cleaning
+test_labels = test_data["HomeWin"]
+test_labels = test_labels.to_numpy(dtype=bool)
+test_data = test_data.drop(columns=["HomeWin"])
+
+test_data = test_data.drop(columns=['Visitor/Neutral'])
+train_data = train_data.drop(columns=['Visitor/Neutral'])
+
+row_t, col_t = train_data.shape
+
+# converting data into arrays
+train_data = train_data.values
+test_data = test_data.values
+
+# creating the model
+model = keras.Sequential([
+    keras.layers.Dense(col_t-1),
+    keras.layers.Dense(256, activation='tanh'),
+    keras.layers.Dense(128, activation='tanh'),
+    keras.layers.Dense(2, activation='softmax')
+])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.fit(train_data, train_labels, epochs=5)
+model.evaluate(test_data,test_labels)
